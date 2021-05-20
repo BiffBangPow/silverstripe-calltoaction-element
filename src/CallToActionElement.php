62d300e38b5fb39eda66061a5297fc329553a9ad
Tim Burt <tim@biffbangpow.com>
2021-05-20 11:30:25 +0100

    Initial update of module

---
+++ src/CallToActionElement.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace BiffBangPow\Element;
+
+use DNADesign\Elemental\Models\BaseElement;
+use Sheadawson\Linkable\Forms\LinkField;
+use Sheadawson\Linkable\Models\Link;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
+class CallToActionElement extends BaseElement
+{
+    /**
+     * @var string
+     */
+    private static $table_name = 'ElementCallToAction';
+
+    private static $singular_name = 'call to action element';
+
+    private static $plural_name = 'call to action elements';
+
+    private static $description = 'Text with a call to action button';
+
+    /**
+     * @var array
+     */
+    private static $db = [
+        'Text' => 'HTMLText',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $has_one = [
+        'CallToAction' => Link::class,
+    ];
+
+    /**
+     * @var array
+     */
+    private static $owns = [
+        'CallToAction',
+    ];
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldsToTab(
+            'Root.Main',
+            [
+                HTMLEditorField::create('Text'),
+                LinkField::create(
+                    'CallToActionID',
+                    'Call To Action'
+                ),
+            ]
+        );
+
+        return $fields;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return 'Call to Action';
+    }
+
+    public function getSimpleClassName()
+    {
+        return 'call-to-action-element';
+    }
+}
---
+++ templates/BiffBangPow/Element/CallToActionElement.ss
@@ -0,0 +1,12 @@
+<div class="py-4 py-xl-7">
+    <div class="container">
+        <div class="col-12 text-center">
+            <% if $ShowTitle %>
+                <h3 class="mb-4">$MarkdownText.Title.RAW</h3>
+            <% end_if %>
+            <div class="mb-4">$Text</div>
+            <% include Link %>
+        </div>
+    </div>
+</div>
+
