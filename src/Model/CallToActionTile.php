b6d5237916a9f2e7d65b8a0b29ed09d9881bcb10
Tim Burt <tim@biffbangpow.com>
2022-11-23 15:45:27 +0000

    Add display logic into element class

--- src/Model/CallToActionTile.php
+++ src/Model/CallToActionTile.php
@@ -16,6 +16,7 @@ use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\TreeDropdownField;
 use SilverStripe\ORM\DataObject;
+use UncleCheese\DisplayLogic\Forms\Wrapper;
 
 
 class CallToActionTile extends DataObject
@@ -52,6 +53,23 @@ class CallToActionTile extends DataObject
                 ->setAllowedFileCategories('image/supported')
                 ->setFolderName('CTA')
         ]);
+
+        $fields->addFieldsToTab('Root.Main', [
+            DropdownField::create('CTAType', 'Call to action type',
+                singleton($this->owner->ClassName)->dbObject('CTAType')->enumValues()),
+            Wrapper::create(TreeDropdownField::create('ActionID', 'Link to page', SiteTree::class))
+                ->displayIf("CTAType")->isEqualTo("Link")->end(),
+            TextField::create('LinkAnchor')
+                ->setDescription('Anchor to append to the end of the link (do not need to include #)')
+                ->displayIf("CTAType")->isEqualTo("Link")->end(),
+            UploadField::create('DownloadFile', 'Download File')->setFolderName('Downloads')
+                ->displayIf('CTAType')->isEqualTo("Download")->end(),
+            TextField::create('LinkData')
+                ->setDescription('External link, email address, etc.')
+                ->displayIf("CTAType")->isEqualTo("External")
+                ->orIf("CTAType")->isEqualTo("Email")->end(),
+            TextField::create('LinkText')->displayUnless("CTAType")->isEqualTo("None")->end(),
+        ]);        
 
         return $fields;
     }
